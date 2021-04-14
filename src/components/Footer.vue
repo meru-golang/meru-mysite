34ab9579134a08985a152a4f78b827f87e667d4e
める <82102170+meru-golang@users.noreply.github.com>
2021-04-14 18:01:08 +0900

    Add files via upload

---
+++ src/components/Footer.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="footer">
+    <p class="title">&copy;meru_golang</p>
+  </div>
+</template>
+
+<style>
+.footer{
+  margin-top: 100px;
+    background-color: #ADD8E6;
+}
+.title{
+    padding:20px;
+    color: whitesmoke;
+}
+
+</style>
---
+++ src/components/Skill.vue
@@ -0,0 +1,106 @@
+<template>
+    <div>
+        <h1 class="content-title"><font-awesome-icon icon="code" class="icon"/> Skill</h1>
+        <div>
+            <h2><font-awesome-icon icon="file-code" class="icon"/> languages</h2>
+            <div class="items">
+                <div class="item">
+                    <img src="https://blog.golang.org/go-brand/Go-Logo/SVG/Go-Logo_LightBlue.svg" alt="logo" class="item-image">
+                    <h3>Golang</h3>
+                </div>
+                <div class="item">
+                    <img src="../assets/lang/ruby-lang-icon.svg" alt="logo" class="item-image">
+                    <h3>Ruby</h3>
+                </div>
+                <div class="item">
+                    <img src="../assets/lang/unity3d-icon.svg" alt="logo" class="item-image">
+                    <h3>Unity</h3>
+                </div>
+                <div class="item">
+                    <img src="../assets/lang/dart.svg" alt="logo" class="item-image">
+                    <h3>Dart</h3>
+                </div>
+                <div class="item">
+                    <img src="../assets/lang/C++.svg" alt="logo" class="item-image">
+                    <h3>C++</h3>
+                </div>
+                <div class="item">
+                    <img src="../assets/lang/javascript.svg" alt="logo" class="item-image">
+                    <h3>JavaScript</h3>
+                </div>
+                <div class="item">
+                    <img src="../assets/lang/typescript.svg" alt="logo" class="item-image">
+                    <h3>TypeScript</h3>
+                </div>
+            </div>
+            
+
+            <h2><font-awesome-icon icon="archive" class="icon"/> frameworks</h2>
+            <div class="items">
+                <div class="item">
+                    <img src="../assets/lang/vue.svg" alt="logo" class="item-image">
+                    <h3>Vue.js</h3>
+                </div>
+                <div class="item">
+                    <img src="../assets/lang/nuxt.svg" alt="logo" class="item-image">
+                    <h3>Nuxt.js</h3>
+                </div>
+                <div class="item">
+                    <img src="../assets/lang/flutter-icon.svg" alt="logo" class="item-image">
+                    <h3>Flutter</h3>
+                </div>
+            </div>
+
+            <h2><font-awesome-icon icon="toolbox" class="icon"/> others</h2>
+            <div class="items">
+                <div class="item">
+                    <img src="../assets/lang/git.svg" alt="logo" class="item-image">
+                    <h3>Git</h3>
+                </div>
+                <div class="item">
+                    <img src="../assets/lang/github.svg" alt="logo" class="item-image">
+                    <h3>GitHub</h3>
+                </div>
+                <div class="item">
+                    <img src="../assets/lang/nginx-icon.svg" alt="logo" class="item-image">
+                    <h3>Nginx</h3>
+                </div>
+                <div class="item">
+                    <img src="../assets/lang/docker-official.svg" alt="logo" class="item-image">
+                    <h3>Docker</h3>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+
+<style>
+.items{
+    text-align: center;
+    padding-left: 15%;
+    padding-right: 15%;
+    margin-bottom: 50px;
+}
+
+.item{
+    margin :10px 30px;
+    display: inline-block;
+    width: 100px;
+}
+
+.item-comment{
+    width: 100px;
+}
+
+.item-image{
+    height: 50px;
+    width: 50px;
+}
+
+
+
+
+</style>
+
+
