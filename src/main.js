e0ce914c4f606ff1996ffcb8b1db0692eea70fa3
める <82102170+meru-golang@users.noreply.github.com>
2021-04-14 18:00:44 +0900

    Add files via upload

---
+++ src/main.js
@@ -0,0 +1,27 @@
+import Vue from 'vue'
+import App from './App.vue'
+import smoothScroll from 'vue-smoothscroll'
+import VueTyperPlugin from 'vue-typer'
+Vue.use(smoothScroll)
+Vue.use(VueTyperPlugin)
+
+import { library } from '@fortawesome/fontawesome-svg-core'
+
+import { faHandshake,faCode,faUserCircle,faLink,faPen,faSchool,faHeart,
+  faSearch,faEnvelope,
+  faBirthdayCake,faImage,faArchive,faToolbox,faFileCode } from '@fortawesome/free-solid-svg-icons'
+
+import { faTwitter,faGithub,faJsSquare } from '@fortawesome/free-brands-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+library.add(faHandshake,faCode,faUserCircle,faLink,faPen,
+  faSchool,faHeart,faSearch,faTwitter,faGithub,faEnvelope,
+  faBirthdayCake,faJsSquare,faImage,faToolbox,faArchive,faFileCode)
+
+Vue.component('font-awesome-icon', FontAwesomeIcon)
+
+Vue.config.productionTip = false
+
+new Vue({
+  render: h => h(App),
+}).$mount('#app')
